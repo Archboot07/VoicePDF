9c11c497d2eddc056e0b7bb9823cc40d56956c7a
Arch_0x3f5 <104111031+Archboot07@users.noreply.github.com>
2023-09-30 16:07:35 -0300

    Update VoicePDF.py
    A proposta de atualização adicionará uma funcionalidade de escolha da página inicial ao código existente. Isso permitirá que os usuários determinem uma página a partir da qual desejam começar a leitura de um documento ou livro.

--- VoicePDF.py
+++ VoicePDF.py
@@ -28,29 +28,36 @@ class Painel:
             print(f"Erro")
             return None
 
-    # Leitura do PDF
-    def ler_pdf(self, caminho_pdf):
+    # Leitura do PDF a partir de uma página específica
+    def ler_pdf(self, caminho_pdf, pagina_inicio):
         try:
             pdf = PdfReader(caminho_pdf)
             numero_de_paginas = len(pdf.pages)
             print(f'O PDF possui {numero_de_paginas} páginas.')
+            print("\nAlguarde, leitura iniciada...")
+
+            if pagina_inicio < 1 or pagina_inicio > numero_de_paginas:
+                print("Número de página inválido.")
+                return None
+
             texto = []
 
-            # Extraindo texto
-            for pagina in pdf.pages:
-                texto.append(pagina.extract_text())
+            # Extraindo texto a partir da página especificada
+            for i in range(pagina_inicio - 1,numero_de_paginas):
+                texto.append(pdf.pages[i].extract_text())
+        
             return texto
 
-        except:
-            print(f"Erro ao ler o PDF")
+        except Exception as e:
+            print(f"Erro ao ler o PDF: {str(e)}")
             return None
 
     # Função de geração de voz
     def gerar_voz(self, texto):
         try:
-            set_api_key("SUA API KEY ELEVENLABS")  # Substitua pela sua chave da ElevenLab
+            set_api_key(">>>>>>>>SUA API KEY<<<<<<<<<<<<<<")  # Substitua pela sua chave da ElevenLab
             
-            num_caracteres_por_request = 500
+            num_caracteres_por_request = 200  #numero de caracteres para o request
             inicio_caracter = 0
             fim_caracter = num_caracteres_por_request
 
@@ -69,21 +76,23 @@ class Painel:
         except:
             print(f"Erro ao gerar a voz:")
 
-
 if __name__ == "__main__":
     painel = Painel()
 
     # Obter o caminho do PDF
     caminho_do_pdf = painel.pdf()
 
-    #se obteve o caminho, texto das paginas recebe a função ler 
+    # Se obteve o caminho, solicitar a página de início
     if caminho_do_pdf:
-        texto_das_paginas = painel.ler_pdf(caminho_do_pdf)
+        pdf = PdfReader(caminho_do_pdf)
+        numero_de_paginas = len(pdf.pages)
+        pagina_inicio = int(input(f'Digite o número da página de início (1-{numero_de_paginas}): '))
+        texto_das_paginas = painel.ler_pdf(caminho_do_pdf, pagina_inicio)
 
-    #recebe o texto das paginas e da um join
+    # Recebe o texto das páginas e dá um join
     if texto_das_paginas:
         texto_completo = "\n".join(texto_das_paginas)
 
-        # Processar o texto em partes de 500 caracteres por request, mas esta lento deve ser melhorado 
+        #gera o texto em 200 caracteres 
         painel.gerar_voz(texto_completo)
 
